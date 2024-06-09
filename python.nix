6fc700a8efb70fcee3edb4f0c3f40a0a74a58f03
jon-edward <townsend.jonathan.e@gmail.com>
2024-06-09 11:41:06 -0400

    Add python shell

---
+++ python.nix
@@ -0,0 +1,13 @@
+let
+  unstable = import (fetchTarball "https://releases.nixos.org/nixos/unstable/nixos-22.11pre421761.448a599c499/nixexprs.tar.xz") { };
+in
+{ nixpkgs ? import <nixpkgs> {} }:
+
+nixpkgs.mkShell {
+  nativeBuildInputs = with nixpkgs; [
+    unstable.python310
+    unstable.python310Packages.poetry
+  ];
+
+  LD_LIBRARY_PATH = "${nixpkgs.stdenv.cc.cc.lib}/lib";
+}
